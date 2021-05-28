9dc786fb9fc35fc03b4370e154886c2f84d0166c
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-05-28 02:55:55 +0200

    Form Inputs onfocus and onblur added

---
+++ twitter/script.js
@@ -0,0 +1,29 @@
+
+
+var placeholder1 = document.getElementById("placeholder1");
+var placeholder2 = document.getElementById("placeholder2");
+
+function fc1()
+{
+    placeholder1.style.fontSize = "0.9em";
+    placeholder1.style.top = "-70px";
+}
+
+function fc2()
+{
+    placeholder2.style.fontSize = "0.9em";
+    placeholder2.style.top = "-70px";
+}
+
+function fb1()
+{
+    
+    placeholder1.style.fontSize = "1.2em";
+    placeholder1.style.top = "-44px";
+}
+
+function fb2()
+{
+    placeholder2.style.fontSize = "1.2em";
+    placeholder2.style.top = "-44px";
+}
